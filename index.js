c3d5dbc1959966c7d42a6463c93becb34a3486a7
jairomatheus <jairomatheus89@outlook.com>
2024-12-24 01:36:21 -0300

    i dont know

---
+++ index.js
@@ -0,0 +1,17 @@
+// Importa o módulo express
+const path = require('path');
+const express = require('express');
+
+// Cria uma aplicação Express
+const app = express();
+
+// Define uma rota para a página inicial
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname,'paginaMain.html'));
+});
+
+// Configura a aplicação para escutar na porta 3000
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
