57e9887b40c86b2ff8bd80410b9216c73cd22710
root <root@srv677805.hstgr.cloud>
2024-12-28 19:05:22 +0000

    yeah

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 // Importa o módulo express
 const path = require('path');
 const express = require('express');
-
 // Cria uma aplicação Express
 const app = express();
 
+// Configuração para servir arquivos estáticos da pasta atual
+app.use(express.static(path.join(__dirname)));
+
 // Define uma rota para a página inicial
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname,'./paginaMain.html'));
+    res.sendFile(path.join(__dirname,'./public/paginaMain.html'));
 });
 
 // Configura a aplicação para escutar na porta 3000
